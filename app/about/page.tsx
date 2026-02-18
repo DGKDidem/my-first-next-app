52a0ae0cf4f42c9dd8d5caf2ad38bc258be63f4c
DGKDidem <dgk.didem92@gmail.com>
2026-02-18 21:18:12 +0300

    Updated home page and added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-2xl font-semibold">
+        Bu benim About sayfam 👩‍💻
+      </h1>
+    </main>
+  );
+}
